99b9d705f65d967fc7fcefa9e689a838e37a914d
José Gabriel González <jgg032899@gmail.com>
2019-10-22 16:46:53 -0400

    Added Bootstrap and JQuery

--- src/main/java/edu/pucmm/sparkjdbc/Main.java
+++ src/main/java/edu/pucmm/sparkjdbc/Main.java
@@ -1,7 +1,9 @@
 package edu.pucmm.sparkjdbc;
 
+import static spark.Spark.staticFiles;
+
 public class Main {
     public static void main(String[] args) {
-
+        staticFiles.location("/publico");
     }
 }
