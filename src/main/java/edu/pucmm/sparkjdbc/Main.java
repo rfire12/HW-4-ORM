0c32b61a07c72a7ea9ac0f67e392e3d7fc8b7539
Raul Paulino <rapraulra@gmail.com>
2019-10-23 18:49:06 -0400

    Creating templates

--- src/main/java/edu/pucmm/sparkjdbc/Main.java
+++ src/main/java/edu/pucmm/sparkjdbc/Main.java
@@ -7,8 +7,20 @@ import java.sql.SQLException;
 
 import static spark.Spark.staticFiles;
 
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.Map;
+
+import static spark.Spark.*;
+
+
 public class Main {
+    public static String renderFreemarker(Map<String, Object> model, String templatePath) {
+        return new FreeMarkerEngine().render(new ModelAndView(model, templatePath));
+    }
     public static void main(String[] args) throws SQLException {
+
         staticFiles.location("/publico");
 
         // Iniciando el servicio
@@ -19,5 +31,9 @@ public class Main {
 
         // Probando conexión
         DataBaseServices.getInstance().testConnection();
+
+        get("/", (request, response) -> {
+            return renderFreemarker(null, "index.ftl");
+        });
     }
 }
