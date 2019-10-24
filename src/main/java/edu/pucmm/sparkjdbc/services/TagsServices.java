a2de305a539f368186d854a33126c1a7b68a9c43
José Gabriel González <jgg032899@gmail.com>
2019-10-23 23:35:03 -0400

    Merge 'services' into master

--- src/main/java/edu/pucmm/sparkjdbc/services/TagsServices.java
+++ src/main/java/edu/pucmm/sparkjdbc/services/TagsServices.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class TagsServices {
@@ -17,6 +19,33 @@ public class TagsServices {
             instance = new TagsServices();
         }
         return instance;
+    }
+
+    public List<Tag> getTags() {
+        List<Tag> tags = new ArrayList<>();
+        Connection con = null;
+        try {
+            String query = "select * from tags";
+            con = DataBaseServices.getInstance().getConnection();
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                Tag tag = new Tag();
+                tag.setUid(rs.getString("uid"));
+                tag.setTag(rs.getString("tag"));
+
+                tags.add(tag);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return tags;
     }
 
     public Tag getTag(long uid) {
