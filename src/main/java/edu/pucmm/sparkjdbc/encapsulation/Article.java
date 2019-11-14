f8c17d92b50b72d9dbc7a645f7d060c7c4259a8a
José Gabriel González <jgg032899@gmail.com>
2019-11-13 20:36:38 -0400

    Update Article.java

--- src/main/java/edu/pucmm/sparkjdbc/encapsulation/Article.java
+++ src/main/java/edu/pucmm/sparkjdbc/encapsulation/Article.java
@@ -17,7 +17,7 @@ public class Article implements Serializable {
     private Timestamp date;
     @OneToMany(mappedBy = "article", fetch = FetchType.EAGER)
     private Set<Comment> comments;
-    @ManyToMany(mappedBy = "articles", fetch = FetchType.EAGER)
+    @ManyToMany
     private Set<Tag> tags;
     private int likes;
     private int dislikes;
